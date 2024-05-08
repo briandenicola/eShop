61913dbe2e94b0c460cc83d386b313fdb88f413d
briandenicola <brian@briandenicola.com>
2024-05-08 13:47:46 -0500

    🏐 Wed May  8 13:47:46 CDT 2024

--- src/eShop.ServiceDefaults/Extensions.cs
+++ src/eShop.ServiceDefaults/Extensions.cs
@@ -106,21 +106,22 @@ public static partial class Extensions
     public static WebApplication MapDefaultEndpoints(this WebApplication app)
     {
         // Uncomment the following line to enable the Prometheus endpoint (requires the OpenTelemetry.Exporter.Prometheus.AspNetCore package)
-        // app.MapPrometheusScrapingEndpoint();
+        app.MapPrometheusScrapingEndpoint();
+        app.MapHealthChecks("/health");
 
         // Adding health checks endpoints to applications in non-development environments has security implications.
         // See https://aka.ms/dotnet/aspire/healthchecks for details before enabling these endpoints in non-development environments.
-        if (app.Environment.IsDevelopment())
-        {
-            // All health checks must pass for app to be considered ready to accept traffic after starting
-            app.MapHealthChecks("/health");
+        // if (app.Environment.IsDevelopment())
+        // {
+        //     // All health checks must pass for app to be considered ready to accept traffic after starting
+        //     app.MapHealthChecks("/health");
 
-            // Only health checks tagged with the "live" tag must pass for app to be considered alive
-            app.MapHealthChecks("/alive", new HealthCheckOptions
-            {
-                Predicate = r => r.Tags.Contains("live")
-            });
-        }
+        //     // Only health checks tagged with the "live" tag must pass for app to be considered alive
+        //     app.MapHealthChecks("/alive", new HealthCheckOptions
+        //     {
+        //         Predicate = r => r.Tags.Contains("live")
+        //     });
+        // }
 
         return app;
     }
